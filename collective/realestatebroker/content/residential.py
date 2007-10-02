59168f89d7e7f1251a6da058ed957351a4dde0cb
Reinout van Rees <reinout@vanrees.org>
2007-10-02 18:53:58 +0000

    added registerType() call
    svn path=/collective.realestatebroker/trunk/; revision=50671

--- collective/realestatebroker/content/residential.py
+++ collective/realestatebroker/content/residential.py
@@ -1,6 +1,10 @@
 """Schema and content type for residential real estate."""
 from Products.Archetypes import atapi
+from collective.realestatebroker import PROJECTNAME
 
 
 class Residential(atapi.BaseFolder):
     """Folderish content type for residential real estate."""
+
+
+atapi.registerType(Residential, PROJECTNAME)
