2b9319ba2a7ab046b5b23c3e8ce19a6eec1fe426
Fred van Dijk <f.vandijk@zestsoftware.nl>
2007-10-03 14:29:46 +0000

    Added additional fields for the residential content type in a separate schemata.py
    svn path=/collective.realestatebroker/trunk/; revision=50729

--- collective/realestatebroker/content/residential.py
+++ collective/realestatebroker/content/residential.py
@@ -2,11 +2,17 @@
 from Products.Archetypes import atapi
 from collective.realestatebroker.config import PROJECTNAME
 
+from collective.realestatebroker.content.schemata import GeneralInfoSchema
 
 class Residential(atapi.BaseFolder):
     """Folderish content type for residential real estate."""
     portal_type = "Residential"
     _at_rename_after_creation = True
+    schema = atapi.BaseFolderSchema.copy() + GeneralInfoSchema
+    
+    # temporary vocabulary for selectionwidgets in Schemas
+    def _get_dummy_vocab(self):
+        return ("choice 1","choice 2","choice 3")
 
-
+  
 atapi.registerType(Residential, PROJECTNAME)
