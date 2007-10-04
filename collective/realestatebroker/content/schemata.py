055b37d4edda6d2dca1b3c66d7de83d28dc223fe
Reinout van Rees <reinout@vanrees.org>
2007-10-04 13:48:24 +0000

    Indentation fixed. Removed not-that-useful descriptions.
    svn path=/collective.realestatebroker/trunk/; revision=50818

--- collective/realestatebroker/content/schemata.py
+++ collective/realestatebroker/content/schemata.py
@@ -3,70 +3,63 @@ from Products.Archetypes import atapi
 from collective.realestatebroker import RealEstateBrokerMessageFactory as _
 
 
-
 GeneralInfoSchema = atapi.Schema((
 
     atapi.TextField('address',
-                index='FieldIndex',
-                schemata='General info',
-                widget=atapi.StringWidget(label = _(u"label_address", default=u"Address"),
-                                description = _(u'desc_address', default=u"Fill in the address of this object"),
-                                size = 30,
-                                )
-                ),
+                    index='FieldIndex',
+                    schemata='General info',
+                    widget=atapi.StringWidget(label = _(u"label_address", default=u"Address"),
+                                              size = 30,
+                                              )
+                    ),
 
     atapi.TextField('zipCode',
-                index='TextIndex',
-                schemata='General info',            
-                widget=atapi.StringWidget(label = _(u"label_zipCode", default=u"Zip code"),
-                                description = _(u"desc_zipCode", default=u"Fill in the zip code of this object"),
-                                size = 30,
-                                )
-                ),
+                    index='TextIndex',
+                    schemata='General info',
+                    widget=atapi.StringWidget(label = _(u"label_zipCode", default=u"Zip code"),
+                                              size = 30,
+                                              )
+                    ),
     atapi.TextField('city',
-                index='TextIndex',
-                schemata='General info',
-                vocabulary='_get_dummy_vocab',            
-                widget=atapi.SelectionWidget(label = _(u"label_city", default=u"City"),
-                            description = _(u"desc_city", default=u"Fill in the city in which this object is located."),
-                            size = 30,
-                            )
-                ),            
+                    index='TextIndex',
+                    schemata='General info',
+                    vocabulary='_get_dummy_vocab',
+                    widget=atapi.SelectionWidget(label = _(u"label_city", default=u"City"),
+                                                 size = 30,
+                                                 )
+                    ),
 
     atapi.TextField('price',
-                index='FieldIndex',
-                schemata='General info',            
-                widget=atapi.IntegerWidget(label = _(u"label_price", default=u"Price"),
-                            description = _(u"desc_price", default=u"Fill in the price without dots or comma's."),
-                            size = 30,
-                            )
-                ),
+                    index='FieldIndex',
+                    schemata='General info',
+                    widget=atapi.IntegerWidget(label = _(u"label_price", default=u"Price"),
+                                               size = 30,
+                                               )
+                    ),
 
     atapi.TextField('kk_von',
-                index='TextIndex',
-                schemata='General info',
-                vocabulary='_get_dummy_vocab',
-                widget=atapi.SelectionWidget(label = _(u"label_kk_van",default=u"K.k./V.o.n."),
-                            description = _(u"desc_kk_van", default=u"Select the one option."),
-                            )
-                ),
+                    index='TextIndex',
+                    schemata='General info',
+                    vocabulary='_get_dummy_vocab',
+                    widget=atapi.SelectionWidget(label = _(u"label_kk_van",default=u"K.k./V.o.n."),
+                                                 )
+                    ),
 
 
     atapi.TextField('housetype',
-               index='FieldIndex',
-               schemata='General info',
-               vocabulary='_get_dummy_vocab',
-               widget=atapi.SelectionWidget(label = _(u"label_type",default=u"Type"),
-                            description = _(u"desc_type", default=u"Select the type object"),
-                            )
-               ),
+                    index='FieldIndex',
+                    schemata='General info',
+                    vocabulary='_get_dummy_vocab',
+                    widget=atapi.SelectionWidget(label = _(u"label_type",default=u"Type"),
+                                                 description = _(u"desc_type", default=u"Select the type object"),
+                                                 )
+                    ),
 
     atapi.TextField('rooms',
-               index='FieldIndex',
-               schemata='General info',
-               vocabulary='_get_dummy_vocab',
-               widget=atapi.SelectionWidget(label = _(u"label_rooms", default=u"Rooms"),
-                            description= _(u"desc_rooms", default=u"Select the number of rooms for this object"),
-                            )
-               ),
-))
+                    index='FieldIndex',
+                    schemata='General info',
+                    vocabulary='_get_dummy_vocab',
+                    widget=atapi.SelectionWidget(label = _(u"label_rooms", default=u"Rooms"),
+                                                 )
+                    ),
+    ))
