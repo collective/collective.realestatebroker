a940bd1c0ebc219739979982c7e9216ad69f0b5a
Reinout van Rees <reinout@vanrees.org>
2007-10-04 13:51:04 +0000

    whitespace
    svn path=/collective.realestatebroker/trunk/; revision=50819

--- collective/realestatebroker/content/residential.py
+++ collective/realestatebroker/content/residential.py
@@ -1,18 +1,18 @@
 """Schema and content type for residential real estate."""
 from Products.Archetypes import atapi
 from collective.realestatebroker.config import PROJECTNAME
-
 from collective.realestatebroker.content.schemata import GeneralInfoSchema
+
 
 class Residential(atapi.BaseFolder):
     """Folderish content type for residential real estate."""
     portal_type = "Residential"
     _at_rename_after_creation = True
     schema = atapi.BaseFolderSchema.copy() + GeneralInfoSchema
-    
+
     # temporary vocabulary for selectionwidgets in Schemas
     def _get_dummy_vocab(self):
-        return ("choice 1","choice 2","choice 3")
+        return ("choice 1", "choice 2", "choice 3")
 
-  
+
 atapi.registerType(Residential, PROJECTNAME)
