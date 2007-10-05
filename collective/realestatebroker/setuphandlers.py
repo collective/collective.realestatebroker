81cadd3a6c34529f2110eb5b5e5ff705eb40692a
Reinout van Rees <reinout@vanrees.org>
2007-10-05 07:50:37 +0000

    Adding automatic migration of old content
    svn path=/collective.realestatebroker/trunk/; revision=50888

--- collective/realestatebroker/setuphandlers.py
+++ collective/realestatebroker/setuphandlers.py
@@ -3,8 +3,9 @@
 
 
 def importVarious(context):
-    # This is the main step that is called by genericsetup.
-    # Only run step if a flag file is present.
+    """This is the main step that is called by genericsetup."""
+    # Only run step if a flag file is present, otherwise it run every time
+    # that generic setup runs all steps of *any* profile.
     if context.readDataFile('realestatebroker.txt') is None:
         return
     site = context.getSite()
@@ -14,5 +15,9 @@ def importVarious(context):
 def migrate_old_content(site, logger):
     try:
         import Products.contentmigration
+        Products.contentmigration # PYFLAKES
     except:
         return
+    from collective.realestatebroker.migration import migrate
+    result = migrate(site)
+    logger.info(result)
