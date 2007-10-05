21b579e8e91c182f30669c3f38357871f210e9bc
Reinout van Rees <reinout@vanrees.org>
2007-10-05 12:26:03 +0000

    switching to ORDEREDbasefolder
    svn path=/collective.realestatebroker/trunk/; revision=50910

--- collective/realestatebroker/content/commercial.py
+++ collective/realestatebroker/content/commercial.py
@@ -4,11 +4,11 @@ from collective.realestatebroker.config import PROJECTNAME
 from collective.realestatebroker.content.schemata import GeneralInfoSchema
 
 
-class Commercial(atapi.BaseFolder):
+class Commercial(atapi.OrderedBaseFolder):
     """Folderish content type for commercial real estate."""
     portal_type = "Commercial"
     _at_rename_after_creation = True
-    schema = atapi.BaseFolderSchema.copy() + GeneralInfoSchema
+    schema = atapi.OrderedBaseFolderSchema.copy() + GeneralInfoSchema
 
     # temporary vocabulary for selectionwidgets in Schemas
     def _get_dummy_vocab(self):
