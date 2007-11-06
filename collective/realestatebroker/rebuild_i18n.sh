b3a36970635d5326681ff98dec4db0f233046cff
Jean-Paul Ladage <j.ladage@zestsoftware.nl>
2007-11-06 10:14:13 +0000

    From now on we merge the manual.pot because of the Workflow states. Ran rebuild_i18n.sh
    svn path=/collective.realestatebroker/trunk/; revision=53221

--- collective/realestatebroker/rebuild_i18n.sh
+++ collective/realestatebroker/rebuild_i18n.sh
@@ -3,7 +3,7 @@ PRODUCTNAME='collective.realestatebroker'
 I18NDOMAIN=$PRODUCTNAME
 
 # Synchronise the .pot with the templates.
-i18ndude rebuild-pot --pot locales/${PRODUCTNAME}.pot --create ${I18NDOMAIN} .
+i18ndude rebuild-pot --pot locales/${PRODUCTNAME}.pot --merge locales/${PRODUCTNAME}-manual.pot --create ${I18NDOMAIN} .
 
 # Synchronise the resulting .pot with the .po files
 i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/nl/LC_MESSAGES/${PRODUCTNAME}.po
