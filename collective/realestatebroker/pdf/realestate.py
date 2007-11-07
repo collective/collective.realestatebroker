a03c083c23e6871cbb3f0f288d335223e8cb126c
Reinout van Rees <reinout@vanrees.org>
2007-11-07 10:28:21 +0000

    characteristics are now a table
    svn path=/collective.realestatebroker/trunk/; revision=53329

--- collective/realestatebroker/pdf/realestate.py
+++ collective/realestatebroker/pdf/realestate.py
@@ -1,4 +1,4 @@
-from reportlab.platypus import Paragraph, PageBreak, Image
+from reportlab.platypus import Paragraph, PageBreak, Image, Table
 from StringIO import StringIO
 
 from zope.interface import implementer
@@ -72,22 +72,24 @@ def realestateToPDF(context, request):
         structure.append(PageBreak())
 
     # Characteristics
-    structure.append(Paragraph(_(u'Object data'), style['title']))
+    data = []
+    data.append([Paragraph(_(u'Object data'), style['title']), ''])
     for field in realestate_view.base_fields():
         label = field.widget.Label(context)
         value = unicode(field.getAccessor(context)())
-        structure.append(Paragraph(label, style['Normal']))
-        structure.append(Paragraph(value, style['Normal']))
+        data.append([Paragraph(label, style['Normal']),
+                     Paragraph(value, style['Normal'])])
     for section in realestate_view.characteristic_fields():
         if not section['title']:
             continue
-        structure.append(Paragraph(section['title'], style['title']))
+        data.append([Paragraph(section['title'], style['title']), ''])
         for field in section['fields']:
             label = field.widget.Label(context)
             value = unicode(field.getAccessor(context)())
-            structure.append(Paragraph(label, style['Normal']))
-            structure.append(Paragraph(value, style['Normal']))
-    structure.append(PageBreak())
+            data.append([Paragraph(label, style['Normal']),
+                         Paragraph(value, style['Normal'])])
+    table = Table(data=data)
+    structure.append(table)
 
     # Write it out. (Originally this code used a tempfile, but I guess that
     # that's something that's not handled right in this zope version.
