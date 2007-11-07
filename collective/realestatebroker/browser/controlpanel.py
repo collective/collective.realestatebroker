df7c9d6e26920362235733041deb927c79128f74
Fred van Dijk <f.vandijk@zestsoftware.nl>
2007-11-07 09:42:57 +0000

    Configlet for currency in RealEstateBroker,  take two. Now with the files that should have been added. svn-rookie signs off...
    svn path=/collective.realestatebroker/trunk/; revision=53321

---
+++ collective/realestatebroker/browser/controlpanel.py
@@ -0,0 +1,57 @@
+from zope.interface import Interface
+from zope.component import adapts
+from zope.interface import implements
+from zope.formlib.form import FormFields
+
+from zope.i18nmessageid import MessageFactory
+from zope.schema import TextLine
+
+from plone.app.controlpanel.form import ControlPanelForm
+
+from Products.CMFCore.utils import getToolByName
+from Products.CMFDefault.formlib.schema import SchemaAdapterBase
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+
+_ = MessageFactory('collective.realestatebroker')
+
+
+
+class IRealEstateBrokerSchema(Interface):
+        currency = TextLine(
+                        title=_('label_default_currency',
+                                default=u'Default currency.'),
+                        description=_('help_default_coordinated',
+                                      default=u"Specify the default "
+                                               "currency to be used "
+                                               "for prices."),
+                        default=u'EUR',
+                        required=True,
+                       )
+
+
+class REBControlPanelAdapter(SchemaAdapterBase):
+
+    adapts(IPloneSiteRoot)
+    implements(IRealEstateBrokerSchema)
+
+    def __init__(self, context):
+        super(REBControlPanelAdapter, self).__init__(context)
+        properties = getToolByName(context, 'portal_properties')
+        self.context = properties.realestatebroker_properties
+
+    def get_currency(self):
+        return getattr(self.context, 'currency', '')
+
+    def set_currency(self, value):
+        self.context._updateProperty('currency', value)
+
+    currency = property(get_currency, set_currency)
+
+ 
+
+class RealEstateBrokerControlPanel(ControlPanelForm):
+
+    form_fields = FormFields(IRealEstateBrokerSchema)
+    label = _("RealEstateBroker settings")
+    description = None
+    form_name = _("RealEstateBroker settings")
