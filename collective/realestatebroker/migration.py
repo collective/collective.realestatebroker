efaf0ad8bf9ed4512a6cd1c0833b3a743cf735d5
Reinout van Rees <reinout@vanrees.org>
2007-11-13 20:13:53 +0000

    updated the documentation
    svn path=/collective.realestatebroker/trunk/; revision=53773

--- collective/realestatebroker/migration.py
+++ collective/realestatebroker/migration.py
@@ -1,21 +1,14 @@
 """Migration support for the old 1.0 version to 2.0.
 
-We have to accomplish several tasks:
+The migration does the following:
 
 - Replace old REHome/REBusiness objects with Residential/Commercial objects.
 
 - REHome/REBusiness have CMFPhotoAlbums with CMFPhotos in them, these photos
-  must be moved directly into the (folderish) Residential/Commercial object as
+  are moved directly into the (folderish) Residential/Commercial object as
   regular Images.
 
 - Migrate old workflow states OR old status field to new workflow states.
-
-- Migrate old property sheets to new ones.
-
-- Clean up leftover junk like skin layers and old portal types and two portal
-  actions.
-
-- Remove RealEstateBroker/CMFPhoto(Album) from the quickinstaller.
 
 A unittest that tests out the whole migration mechanism can be found in
 `tests/migration-unittest.txt`.
