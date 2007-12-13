19b7afd2e651930c2a1d9eb3f38b7d7afdfc5cf3
Reinout van Rees <reinout@vanrees.org>
2007-12-13 12:41:13 +0000

    removed unneeded check for migration product: we depend on it in our setup.py
    svn path=/collective.realestatebroker/trunk/; revision=55476

--- collective/realestatebroker/config.py
+++ collective/realestatebroker/config.py
@@ -2,23 +2,6 @@
 """
 
 PROJECTNAME = "collective.realestatebroker"
-try:
-    from Products.contentmigration import walker
-    MIGRATIONPRODUCTAVAILABLE = True
-except:
-    MIGRATIONPRODUCTAVAILABLE = False
-
-#CUSTOM_PORTLET_COLUMN = u"plone.rightcolumn"
-
-# This maps portal types to their corresponding add permissions.
-# These are referenced in the root product __init__.py, during
-# Archetypes/CMF type initialisation. The permissions here are
-# also defined in content/configure.zcml, so that they can be
-# looked up as a Zope 3-style IPermission utility.
-
-# We prefix the permission names with our product name to group
-# them sensibly. This is good practice, because it makes it
-# easier to find permissions in the Security tab in the ZMI.
 
 ADD_PERMISSIONS = {
     "Residential" : "realestatebroker : Add Residential real estate",
