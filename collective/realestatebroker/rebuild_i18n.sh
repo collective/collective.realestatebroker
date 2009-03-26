49e220c4280b322a824b35f8ed5801ea3ec4c5cb
Jean-Paul Ladage <j.ladage@zestsoftware.nl>
2009-03-26 09:06:17 +0000

    Added french translations
    svn path=/collective.realestatebroker/trunk/; revision=83181

--- collective/realestatebroker/rebuild_i18n.sh
+++ collective/realestatebroker/rebuild_i18n.sh
@@ -8,7 +8,9 @@ i18ndude rebuild-pot --pot locales/${PRODUCTNAME}.pot --merge locales/${PRODUCTN
 # Synchronise the resulting .pot with the .po files
 i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/nl/LC_MESSAGES/${PRODUCTNAME}.po
 i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/il/LC_MESSAGES/${PRODUCTNAME}.po
+i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/fr/LC_MESSAGES/${PRODUCTNAME}.po
 
 # compile mo file
 msgfmt -o locales/nl/LC_MESSAGES/${PRODUCTNAME}.mo locales/nl/LC_MESSAGES/${PRODUCTNAME}.po
 msgfmt -o locales/il/LC_MESSAGES/${PRODUCTNAME}.mo locales/il/LC_MESSAGES/${PRODUCTNAME}.po
+msgfmt -o locales/il/LC_MESSAGES/${PRODUCTNAME}.mo locales/fr/LC_MESSAGES/${PRODUCTNAME}.po
